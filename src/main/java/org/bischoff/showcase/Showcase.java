91e0ed5fbed1fccee7110d698e18ca620802fc73
BischoffTV <155627117+BischoffTV@users.noreply.github.com>
2024-01-04 17:57:31 +0100

    Add files via upload
    Update, that implement the function for dropper chests and Dispenser

--- src/main/java/org/bischoff/showcase/Showcase.java
+++ src/main/java/org/bischoff/showcase/Showcase.java
@@ -6,6 +6,8 @@ import com.plotsquared.core.plot.Plot;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.Chest;
+import org.bukkit.block.Dispenser;
+import org.bukkit.block.Dropper;
 import org.bukkit.block.Hopper;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -31,20 +33,30 @@ public class Showcase extends JavaPlugin implements Listener {
         Block block = event.getBlock();
         Player player = event.getPlayer();
 
-        if (block.getType() == Material.TRAPPED_CHEST || block.getType() == Material.HOPPER) {
+        if (block.getType() == Material.TRAPPED_CHEST || block.getType() == Material.HOPPER || block.getType() == Material.DROPPER || block.getType() == Material.DISPENSER || block.getType() == Material.CHEST) {
             PlotPlayer<Player> plotPlayer = PlotPlayer.from(player);
             Plot plot = plotPlayer.getCurrentPlot();
 
             if (plot != null && plot.getOwners().contains(player.getUniqueId())) {
-                if (block.getType() == Material.TRAPPED_CHEST) {
+                if (block.getState() instanceof Chest) {
                     Chest chest = (Chest) block.getState();
                     if ("Showcase".equals(chest.getCustomName())) {
                         chest.update();
                     }
-                } else {
+                } else if (block.getState() instanceof Hopper) {
                     Hopper hopper = (Hopper) block.getState();
                     if ("Showcase".equals(hopper.getCustomName())) {
                         hopper.update();
+                    }
+                } else if (block.getState() instanceof Dropper) {
+                    Dropper dropper = (Dropper) block.getState();
+                    if ("Showcase".equals(dropper.getCustomName())) {
+                        dropper.update();
+                    }
+                } else if (block.getState() instanceof Dispenser) {
+                    Dispenser dispenser = (Dispenser) block.getState();
+                    if ("Showcase".equals(dispenser.getCustomName())) {
+                        dispenser.update();
                     }
                 }
             }
@@ -56,7 +68,7 @@ public class Showcase extends JavaPlugin implements Listener {
         Block block = event.getBlock();
         Player player = event.getPlayer();
 
-        if (block.getType() == Material.TRAPPED_CHEST || block.getType() == Material.HOPPER) {
+        if (block.getType() == Material.TRAPPED_CHEST || block.getType() == Material.HOPPER || block.getType() == Material.DROPPER || block.getType() == Material.DISPENSER || block.getType() == Material.CHEST) {
             PlotPlayer<Player> plotPlayer = PlotPlayer.from(player);
             Plot plot = plotPlayer.getCurrentPlot();
 
@@ -72,9 +84,17 @@ public class Showcase extends JavaPlugin implements Listener {
             Player player = (Player) event.getWhoClicked();
             InventoryHolder holder = event.getInventory().getHolder();
 
-            if (holder instanceof Chest || holder instanceof Hopper) {
-                Block block = event.getInventory().getLocation().getBlock();
-                String customName = block.getType() == Material.TRAPPED_CHEST ? ((Chest) holder).getCustomName() : ((Hopper) holder).getCustomName();
+            if (holder instanceof Chest || holder instanceof Hopper || holder instanceof Dropper || holder instanceof Dispenser) {
+                String customName = null;
+                if (holder instanceof Chest) {
+                    customName = ((Chest) holder).getCustomName();
+                } else if (holder instanceof Hopper) {
+                    customName = ((Hopper) holder).getCustomName();
+                } else if (holder instanceof Dropper) {
+                    customName = ((Dropper) holder).getCustomName();
+                } else if (holder instanceof Dispenser) {
+                    customName = ((Dispenser) holder).getCustomName();
+                }
 
                 if ("Showcase".equals(customName)) {
                     PlotPlayer<Player> plotPlayer = PlotPlayer.from(player);
